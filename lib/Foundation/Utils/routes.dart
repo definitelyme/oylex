91bd0384b08f07a3743af8424bb40e529d9f65f2
f0rx <ejike.br@gmail.com>
2020-04-03 03:18:12 +0100

    Create application's navigation routes

---
+++ lib/Foundation/Utils/routes.dart
@@ -0,0 +1,7 @@
+import 'package:oylex/Screens/auth/login.dart';
+import 'package:oylex/Screens/auth/register-1.dart';
+
+var routes = {
+  "/login": (context) => LoginScreen(),
+  "/register-1": (context) => NewAccountScreen()
+};
