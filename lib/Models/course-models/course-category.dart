ef513781ca7a8e641bdc72948237ca354c795bbd
f0rx <ejike.br@gmail.com>
2020-04-26 00:26:51 +0100

    Cleanup dummydata

--- lib/Models/course-models/course-category.dart
+++ lib/Models/course-models/course-category.dart
@@ -1,16 +1,28 @@
 import 'package:oylex/Models/base-model.dart';
+import 'package:oylex/Models/course-models/course.dart';
 
 class CourseCategory extends Model {
   final String name;
+  List<Course> courses;
 
-  CourseCategory({String id, this.name}) : super(id: id);
+  CourseCategory({
+    String id,
+    this.name,
+    this.courses,
+  }) : super(id: id);
+
+  void addCourse(Course course) {
+    if (courses == null) courses = new List<Course>();
+    if (!courses.any((val) => val.id == course.id)) courses.add(course);
+  }
 
   static List<CourseCategory> dummyCategories() {
     return [
       CourseCategory(id: 'science', name: "Science"),
       CourseCategory(id: 'law', name: "Law"),
-      CourseCategory(id: 'mathematics', name: "Mathmatics"),
+      CourseCategory(id: 'mathematics', name: "Mathematics"),
       CourseCategory(id: 'gst', name: "General Studies"),
+      CourseCategory(id: 'management', name: "Management"),
       CourseCategory(id: 'technology', name: "Technology"),
       CourseCategory(id: 'arts-design', name: "Arts & Design"),
       CourseCategory(id: 'engineering', name: "Engineering"),
