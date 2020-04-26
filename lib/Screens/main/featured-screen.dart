73b672e9191103577a5ba9371f4f0c8d56f4b077
Brendan Ejike <ejike.br@gmail.com>
2020-04-26 01:21:07 +0100

    Add Featured Screen foundation

--- lib/Screens/main/featured-screen.dart
+++ lib/Screens/main/featured-screen.dart
@@ -1,27 +1,33 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class FeaturedScreen extends StatefulWidget {
   static final keyValue = "featured-screen";
 
-  FeaturedScreen({Key key, this.scrollController}) : super(key: key);
-
-  final ScrollController scrollController;
+  FeaturedScreen({Key key}) : super(key: key);
 
   @override
   _FeaturedScreenState createState() => _FeaturedScreenState();
 }
 
 class _FeaturedScreenState extends State<FeaturedScreen> {
+  ScrollController _scrollController;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _scrollController = Provider.of<ScrollController>(context, listen: false);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: ListView.builder(
         physics: BouncingScrollPhysics(),
-        controller: widget.scrollController,
-        itemCount: 80,
-        itemBuilder: (context, index) =>
-            ListTile(title: Text("This is ITEM ${index.toString()}")),
+        controller: _scrollController,
+        itemCount: 150,
+        itemBuilder: (context, index) => ListTile(title: Text("This is ITEM ${index.toString()}")),
       ),
     );
   }
